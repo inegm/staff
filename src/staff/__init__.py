44c38f3f3cac798625ec44b344cfa5f2fd5f4055
Ismail Negm <ismailnegm@protonmail.com>
2023-06-04 11:22:24 +0200

    fixes import ordering

--- src/staff/__init__.py
+++ src/staff/__init__.py
@@ -3,5 +3,5 @@ __version__ = "0.2.0"
 # ruff: noqa
 from . import duration, pitch
 from .duration import Duration, Tempo, Tuplet
-from .pitch import Cents, Diapason, Frequency, MIDIBend, MIDIPitch
 from .patterns import DurationPattern
+from .pitch import Cents, Diapason, Frequency, MIDIBend, MIDIPitch
