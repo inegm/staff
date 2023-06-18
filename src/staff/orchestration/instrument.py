00d871ee27210615456500bbbe3dd419f96a09db
Ismail Negm <ismailnegm@protonmail.com>
2023-06-18 14:02:44 +0200

    adds `to_range` and `get_articulation` methods to `Instrument`

--- src/staff/orchestration/instrument.py
+++ src/staff/orchestration/instrument.py
@@ -39,6 +39,10 @@ class InstrumentRange:
 class Instrument:
     """A musical instrument.
 
+    Note:
+        This class can be stored and retrieved from a database.
+        See the documentation for the `staff.db` module for more information.
+
     Args:
         name: The name of the instrument.
         section: The section the instrument belongs to.
@@ -53,6 +57,8 @@ class Instrument:
         description: The description of the instrument.
     """
 
+    # TODO: Add CC numbers for vibrato, expression, etc.
+
     name: str
     section: str
     range: InstrumentRange
@@ -61,6 +67,14 @@ class Instrument:
     abbreviation: str = ""
     category: str = "Default"
     description: str = ""
+
+    def __repr__(self) -> str:
+        """Return a string representation of the instrument."""
+        return (
+            f"Instrument(name='{self.name}', "
+            f"category='{self.category}', "
+            f"description='{self.description}')"
+        )
 
     def in_range(self, pitch: MIDIPitch) -> bool:
         """Check if a pitch is in the instrument's range.
@@ -72,3 +86,80 @@ class Instrument:
             True if the pitch is in the instrument's range, False otherwise.
         """
         return self.range.bottom <= pitch <= self.range.top
+
+    def to_range(
+        self,
+        pitch: MIDIPitch,
+        close_to: MIDIPitch,
+    ) -> MIDIPitch:
+        """Finds the pitch within the instrument's range, closest to another.
+
+        Args:
+            pitch: The pitch to coerce.
+            close_to: A pitch close to where the resulting pitch should be.
+
+        Returns:
+            The coerced pitch.
+
+        Raises:
+            ValueError: If `close_to` is not in the instrument's range.
+
+        Examples:
+            >>> from staff import MIDIPitch
+            >>> from staff.orchestration.instrument import Instrument, InstrumentRange
+            >>> instrument = Instrument(
+            ...     name="Violin",
+            ...     section="Strings",
+            ...     range=InstrumentRange(
+            ...         bottom=MIDIPitch(55),
+            ...         top=MIDIPitch(103),
+            ...     ),
+            ...     articulations=[],
+            ...     is_continuous=True,
+            ... )
+            >>> low_e = MIDIPitch.from_string("E1")
+            >>> target = MIDIPitch.from_string("E3")
+            >>> target == instrument.to_range(low_e, instrument.range.bottom)
+            True
+        """
+        if close_to > self.range.top:
+            close_to = self.range.top
+        elif close_to < self.range.bottom:
+            close_to = self.range.bottom
+
+        distance = abs(pitch.pitch_class - close_to.pitch_class)
+
+        if distance == 0:
+            coerced_number = close_to.number
+        elif distance < (pitch.octave_divs / 2):
+            coerced_number = close_to.number - distance
+            if coerced_number < self.range.bottom.number:
+                coerced_number = close_to.number + distance
+        else:
+            coerced_number = close_to.number + distance
+            if coerced_number > self.range.top.number:
+                coerced_number = close_to.number - distance
+
+        return MIDIPitch(
+            number=coerced_number,
+            bend=pitch.bend,
+            diapason=pitch.diapason,
+            octave_divs=pitch.octave_divs,
+        )
+
+    def get_articulation(self, name: str) -> Articulation:
+        """Get an articulation by name.
+
+        Args:
+            name: The name of the articulation.
+
+        Returns:
+            The articulation.
+
+        Raises:
+            ValueError: If the articulation does not exist.
+        """
+        for articulation in self.articulations:
+            if articulation.name == name:
+                return articulation
+        raise ValueError(f"Articulation '{name}' does not exist.")
